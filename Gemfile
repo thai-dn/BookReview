f49c605cf836efc62db5854800727a367651e600
balau123 <thai.itplus@gmail.com>
2019-03-31 00:16:14 -0700

    end-part-3

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'simple_form', '~> 4.1'
 gem 'devise', '~> 4.6', '>= 4.6.2'
+gem "paperclip", "~> 6.0.0"
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_025413) do
+ActiveRecord::Schema.define(version: 2019_03_31_064010) do
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2019_03_31_025413) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "category_id"
+    t.string "book_img_file_name"
+    t.string "book_img_content_type"
+    t.integer "book_img_file_size"
+    t.datetime "book_img_updated_at"
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
