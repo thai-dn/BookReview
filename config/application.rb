4bd9d079ce81b057072f25b2c360d1e7748b47ee
balau123 <thai.itplus@gmail.com>
2019-04-06 03:00:18 -0700

    fix env

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,6 @@ module BookReview
   class Application < Rails::Application
     config.load_defaults 5.2
     # config.web_console.whitelisted_ips = '192.168.10.0/24'
-    config.web_console.whiny_requests = false
+    # config.web_console.whiny_requests = false
   end
 end
