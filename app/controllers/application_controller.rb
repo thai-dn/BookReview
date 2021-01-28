a33c15bc4c9b4cfa60fd09a2f3963cfa78342b11
thai-dn <thai.itplus@gmail.com>
2021-01-28 09:05:23 +0700

    fix heroku login error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  after_action :csrf_meta_tags
+  after_action :set_csrf_cookie
 
   def set_csrf_cookie
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
-  end
-
-  def csrf_meta_tags
-    if defined?(protect_against_forgery?) && protect_against_forgery?
-      [
-        tag("meta", name: "csrf-param", content: request_forgery_protection_token),
-        tag("meta", name: "csrf-token", content: form_authenticity_token)
-      ].join("\n").html_safe
-    end
   end
 
   def is_login
